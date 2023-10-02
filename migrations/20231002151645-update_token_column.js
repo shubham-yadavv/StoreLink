a5683a71f64af8c481d66488557cc2d43b101edc
shubham yadav <68185027+shubham-yadavv@users.noreply.github.com>
2023-10-02 23:59:30 +0530

    [db] made changes in migrations

--- migrations/20231002151645-update_token_column.js
+++ migrations/20231002151645-update_token_column.js
@@ -3,24 +3,13 @@
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up (queryInterface, Sequelize) {
-    /**
-     * Add altering commands here.
-     *
-     * Example:
-     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
-     */
     await queryInterface.changeColumn('account', 'token', {
       type: Sequelize.STRING,
-      allowNull: true, // Allow null values for the token field
+      allowNull: true, 
     });
   },
 
   async down (queryInterface, Sequelize) {
-    /**
-     * Add reverting commands here.
-     *
-     * Example:
-     * await queryInterface.dropTable('users');
-     */
+
   }
 };
